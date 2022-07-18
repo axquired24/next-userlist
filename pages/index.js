47026f40f9b8e62725072a472989128c099bb6d7
Albert S <axquired24@gmail.com>
2022-07-18 22:10:44 +0700

    add(fetch): on first load

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
   const [sortBy, setSortBy] = useState('name');
   const [isAsc, setisAsc] = useState(true);
   const [userList, setUserList] = useState([]);
+  const [page, setpage] = useState(1);
   const baseApi = 'https://randomuser.me/api'
   const seedKey = 'albertseed'
   const limit = 10;
@@ -19,6 +20,36 @@ export default function Home() {
   const onSearchInput = (str, gender) => {
     console.log('onSearch', str, gender)
   }
+  
+  const fetchUsers = async () => {
+    const queryGenerator = {
+      'seed': seedKey,
+      'results': limit,
+      'gender': genderFilter,
+      'inc': 'login,name,email,gender,registered',
+      'sort': sortBy,
+      'order': isAsc ? 'asc' : 'desc',
+      'keyword': queryText,
+      'page': page,
+    }
+    const query = Object.entries(queryGenerator).map((q) => {
+      return q[0] + '=' + q[1];
+    }).join('&');
+    const apiUrl = baseApi + '?' + query;
+    
+    try {
+      let userResponse = await fetch(apiUrl)
+      userResponse = userResponse.json()
+      console.log({userResponse})
+    } catch (e) {
+      alert('Error when fetching users data, see log for detail');
+      console.error(e)
+    }
+  }
+  
+  useEffect(() => {
+    fetchUsers()
+  }, []);
   
   return (
     <div className="p-10">
